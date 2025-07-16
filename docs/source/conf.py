09b06096c66e687af859953deb41271882a43df7
carls113 <93612281+carls113@users.noreply.github.com>
2025-07-16 10:09:25 -0400

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,35 +1,30 @@
 # Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
 
-# -- Project information
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'HPC Exchange'
+copyright = '2025, Michael Carlson'
+author = 'Michael Carlson'
+release = '07/16/2025'
 
-release = '0.1'
-version = '0.1.0'
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-# -- General configuration
-
-extensions = [
-    'sphinx.ext.duration',
-    'sphinx.ext.doctest',
-    'sphinx.ext.autodoc',
-    'sphinx.ext.autosummary',
-    'sphinx.ext.intersphinx',
-]
-
-intersphinx_mapping = {
-    'python': ('https://docs.python.org/3/', None),
-    'sphinx': ('https://www.sphinx-doc.org/en/master/', None),
-}
-intersphinx_disabled_domains = ['std']
+extensions = ["sphinx_immaterial"]
 
 templates_path = ['_templates']
+exclude_patterns = []
 
-# -- Options for HTML output
+html_use_index = False
 
-html_theme = 'sphinx_rtd_theme'
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-# -- Options for EPUB output
-epub_show_urls = 'footnote'
+html_theme = 'sphinx_immaterial'
+html_static_path = ['_static']
+html_logo = "_static/rcac.jpeg"
+
